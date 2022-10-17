9b8e4eeb62bd424ce1cf1c4f9fda2bc8436489c7
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-10-17 11:48:09 +0700

    #5 : Quick Link

--- backend/app/Http/Controllers/JobController.php
+++ backend/app/Http/Controllers/JobController.php
@@ -7,22 +7,13 @@ use App\Models\Job;
 
 class JobController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return Job::get();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $job = new Job();
@@ -41,24 +32,13 @@ class JobController extends Controller
         return response()->json(['msg'=>'success']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         $job = Job::findOrFail($id);
@@ -76,12 +56,7 @@ class JobController extends Controller
         return response()->json(['msg'=>'updated']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function destroy($id)
     {
         $job = Job::findOrFail($id);
