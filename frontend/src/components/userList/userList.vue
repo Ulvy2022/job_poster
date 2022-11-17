f81f051341225081a1ba64e5713138244f830b32
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-11-17 09:47:02 +0700

    changed

--- frontend/src/components/userList/userList.vue
+++ frontend/src/components/userList/userList.vue
@@ -46,17 +46,17 @@ export default {
                 }).then((result) => {
                     if (result.isConfirmed) {
                         axios.put("http://localhost:8000/api/user/" + this.idToUpdate, userInfo)
-                        .then((res) => {
-                            if (res.data.msg == 'updated'){
-                                Swal.fire('Saved!', '', 'success')
-                                document.getElementById(this.idToUpdate + 'name').textContent = userInfo.fullName
-                                document.getElementById(this.idToUpdate + 'email').textContent = userInfo.email
-                            } else {
-                                Swal.fire('Something wrong !', '', 'error')
-                            }
-                        }).catch((error) => {
-                            console.log(error);
-                        })
+                            .then((res) => {
+                                if (res.data.msg == 'updated') {
+                                    Swal.fire('Saved!', '', 'success')
+                                    document.getElementById(this.idToUpdate + 'name').textContent = userInfo.fullName
+                                    document.getElementById(this.idToUpdate + 'email').textContent = userInfo.email
+                                } else {
+                                    Swal.fire('Something wrong !', '', 'error')
+                                }
+                            }).catch((error) => {
+                                console.log(error);
+                            })
                     } else if (result.isDenied) {
                         Swal.fire('Changes are not saved', '', 'info')
                     }
@@ -65,10 +65,10 @@ export default {
         },
         getAllUser() {
             axios.get("http://localhost:8000/api/user")
-            .then((res) => {
-                this.allUsers = res.data
-                this.numberOfUsers = this.allUsers.length
-            })
+                .then((res) => {
+                    this.allUsers = res.data
+                    this.numberOfUsers = this.allUsers.length
+                })
         },
         valueSelected(value) {
             this.selected = value;
@@ -183,7 +183,7 @@ export default {
             this.getAllUser();
             this.timeOut = setTimeout(() => {
                 this.isShowTable += 1;
-                if (this.isShowTable == 2) {
+                if (this.isShowTable == 3) {
                     clearTimeout(this.timeOut);
                 }
             }, 1000);
@@ -212,7 +212,7 @@ export default {
     },
     watch: {
         isShowTable() {
-            if (this.isShowTable < 2) {
+            if (this.isShowTable < 3) {
                 this.showTable()
             }
         }
@@ -262,20 +262,26 @@ export default {
         </div>
 
         <!-- end of action and earch bar -->
-        <div role="status" class="w-full flex justify-center items-center" v-if="isShowTable < 2">
-            <svg aria-hidden="true"
-                class="m-auto w-14 h-14 mt-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor" />
-                <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill" />
-            </svg>
-            <span class="sr-only">Loading...</span>
+        <div class="grid grid-cols-1 gap-y-4  rounded-md p-4  w-full mx-auto " v-if="isShowTable < 3">
+            <div class="animate-pulse flex space-x-2 " v-for="user of allUsers" :key="user">
+                <div class="rounded-full bg-slate-200 h-14 w-14"></div>
+                <div class="flex-1 space-y-3 py-1">
+                    <div class="h-4 bg-slate-200 rounded w-24"></div>
+                    <div class="">
+                        <div class="grid grid-cols-3 gap-4">
+                            <div class="h-4 bg-slate-200 rounded "></div>
+                            <div class="h-4 bg-slate-200 rounded "></div>
+                            <div class="h-4 grid grid-cols-3 gap-x-2 rounded ">
+                                <div class="h-4 bg-slate-200 rounded "></div>
+                                <div class="h-4 bg-slate-200 rounded "></div>
+                                <div class="h-4 bg-slate-200 rounded "></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 " v-show="isShowTable == 2">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 " v-show="isShowTable == 3">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="p-4">
@@ -297,7 +303,7 @@ export default {
                 </tr>
             </thead>
             <tbody>
-                <tr class="cursor-pointer bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                <tr class="cursor-pointer bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 "
                     v-for="user of allUsers" :key="user" :id="user.id">
                     <td class="p-4 w-4">
                         <div class="flex items-center">
@@ -322,7 +328,7 @@ export default {
                     <td v-else class="py-4 px-6 capitalize" :id="user.id + 'subscriber'">
                         No
                     </td>
-                    <td class="py-4 px-6 flex justify-evenly text-center">
+                    <td class="py-4 px-6 flex justify-evenly text-center -mt-5">
                         <label for="my-modal-5" @click="getSpecificUser(user.id)"
                             class=" bg-white text-blue-500 hover:underline hover:underline-offset-auto cursor-pointer">Edit
                             User</label>
@@ -404,12 +410,13 @@ export default {
                     </div>
                 </div>
             </form>
-
             <div class="modal-action">
                 <label for="my-modal-5" @click="updateProfile" class="btn">Edit!</label>
             </div>
         </div>
     </div>
+
+
 
     <!-- Put this part before </body> tag -->
 
